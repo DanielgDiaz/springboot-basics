7276a31bfc05d1005454cff2d6a6db0d7cbe5654
DanielgDiaz <danielgdiaz92@gmail.com>
2022-03-23 10:30:27 +0100

    init repository ✨

---
+++ src/main/java/com/danigdiaz/fundamentsspringboot/FundamentsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.danigdiaz.fundamentsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentsSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danigdiaz/fundamentsspringboot/FundamentsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danigdiaz.fundamentsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
