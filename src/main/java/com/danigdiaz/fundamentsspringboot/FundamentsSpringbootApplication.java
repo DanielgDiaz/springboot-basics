8da678af7555dada6c6a5c8c65cf83754d8859c5
DanielgDiaz <danielgdiaz92@gmail.com>
2022-03-23 17:19:38 +0100

    Inyección dependencia Component

--- src/main/java/com/danigdiaz/fundamentsspringboot/FundamentsSpringbootApplication.java
+++ src/main/java/com/danigdiaz/fundamentsspringboot/FundamentsSpringbootApplication.java
@@ -1,13 +1,26 @@
 package com.danigdiaz.fundamentsspringboot;
 
+import com.danigdiaz.fundamentsspringboot.component.ComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FundamentsSpringbootApplication {
+public class FundamentsSpringbootApplication implements CommandLineRunner {
+
+	private ComponentDependency componentDependency;
+
+	public FundamentsSpringbootApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency) {
+		this.componentDependency = componentDependency;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentsSpringbootApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args)  {
+		componentDependency.saludar();
+	}
 }
